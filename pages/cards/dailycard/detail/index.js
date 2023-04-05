4e697b46d958483cd17eae200b3d3cbe851a84a7
rabewtf <rabenklar@gmx.de>
2023-04-05 13:54:44 +0200

    add userstory2

---
+++ pages/cards/dailycard/detail/index.js
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Details({ dailyCard }) {
+  if (dailyCard) {
+    return (
+      <>
+        <h1>{dailyCard.name}</h1>
+        <figure>
+          <Image
+            width="100"
+            height="180"
+            src={dailyCard.image}
+            alt={dailyCard.name}
+          />
+          <figcaption>
+            {dailyCard.type} {dailyCard.value_int}
+          </figcaption>
+        </figure>
+        <section>{dailyCard.meaning_up}</section>
+        <Link
+          href={{
+            pathname: "/cards/dailycard/detail/description",
+          }}
+        >
+          <button type="button">more details</button>
+        </Link>
+      </>
+    );
+  } else {
+    return <p>nope</p>;
+  }
+}
