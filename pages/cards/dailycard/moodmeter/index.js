cf4dd6bdd335f28db66675eb705e7ba13540942e
rabewtf <rabenklar@gmx.de>
2023-04-07 23:44:21 +0200

    make mood/clicks/averageMood available on sites and render instantly

--- pages/cards/dailycard/moodmeter/index.js
+++ pages/cards/dailycard/moodmeter/index.js
@@ -1,30 +1,26 @@
-import useLocalStorageState from "use-local-storage-state";
-import BackButton from "../../../../components/Backbutton/backbutton";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Link from "next/link";
 
-export default function MoodMeter({ dailyCard, id }) {
-  const [mood, setMood] = useLocalStorageState("mood", []);
-  const [clicks, setClicks] = useLocalStorageState("clicks", []);
-  const [averageMood, setAverageMood] = useLocalStorageState("averageMood", []);
+export default function MoodMeter({
+  dailyCard,
+  setMood,
+  setClicks,
+  averageMood,
+  clicks,
+  mood,
+}) {
   const [disableButton, setDisableButton] = useState(false);
 
   function addMood() {
     setMood(mood + 1);
     setClicks(clicks + 1);
-    writeAverageMood();
     setDisableButton(true);
   }
 
   function decreaseMood() {
-    setMood(mood - 1);
+    if (mood > 0) setMood(mood - 1);
     setClicks(clicks + 1);
-    writeAverageMood();
     setDisableButton(true);
-  }
-
-  function writeAverageMood() {
-    setAverageMood(mood / clicks);
   }
 
   return (
@@ -46,7 +42,7 @@ export default function MoodMeter({ dailyCard, id }) {
         +
       </button>
       <p>card: {dailyCard.name}</p>
-      {averageMood ? <p>Mood: {Math.round(averageMood * 100)} %</p> : null}
+      {averageMood >= 0 ? <p>Mood: {Math.round(averageMood * 100)} %</p> : null}
       <p>{averageMood >= 0.5 ? ":)" : ":("}</p>{" "}
       <Link href="../dailycard">
         <button type="button">next</button>
