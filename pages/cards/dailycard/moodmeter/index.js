8b82da4a6c701ecc582c3afb27325507c4e0fdf4
rabewtf <rabenklar@gmx.de>
2023-04-07 22:16:39 +0200

    add button toggle and averageMood

--- pages/cards/dailycard/moodmeter/index.js
+++ pages/cards/dailycard/moodmeter/index.js
@@ -1,34 +1,56 @@
-import useLocalStorage from "use-local-storage";
 import useLocalStorageState from "use-local-storage-state";
 import BackButton from "../../../../components/Backbutton/backbutton";
+import { useEffect, useState } from "react";
+import Link from "next/link";
 
 export default function MoodMeter({ dailyCard, id }) {
-  const [mood, setMood] = useLocalStorageState();
+  const [mood, setMood] = useLocalStorageState("mood", []);
+  const [clicks, setClicks] = useLocalStorageState("clicks", []);
+  const [averageMood, setAverageMood] = useLocalStorageState("averageMood", []);
+  const [disableButton, setDisableButton] = useState(false);
+
+  function addMood() {
+    setMood(mood + 1);
+    setClicks(clicks + 1);
+    writeAverageMood();
+    setDisableButton(true);
+  }
+
+  function decreaseMood() {
+    setMood(mood - 1);
+    setClicks(clicks + 1);
+    writeAverageMood();
+    setDisableButton(true);
+  }
+
+  function writeAverageMood() {
+    setAverageMood(mood / clicks);
+  }
 
   return (
     <>
       <button
+        disabled={disableButton}
         type="button"
         aria-label="minus button"
-        onClick={() => setMood(mood - 1)}
+        onClick={() => decreaseMood()}
       >
         -
       </button>
       <button
+        disabled={disableButton}
         type="button"
         aria-label="plus-button"
-        onClick={() => setMood(mood + 1)}
+        onClick={() => addMood()}
       >
         +
       </button>
-      <p>
-        mood: {mood}
-        <p>card: {dailyCard.name}</p>
-      </p>
-      <p>
-        {" "}
-        <BackButton />
-      </p>{" "}
+      <p>card: {dailyCard.name}</p>
+      {averageMood ? <p>Mood: {Math.round(averageMood * 100)} %</p> : null}
+      <p>{averageMood >= 0.5 ? ":)" : ":("}</p>{" "}
+      <Link href="../dailycard">
+        <button type="button">next</button>
+      </Link>
     </>
   );
 }
