8382fabdeea3b9ec324d10e480676df19af5d28a
rabewtf <rabenklar@gmx.de>
2023-04-09 16:56:19 +0200

    fix bug

--- pages/cards/[id]/index.js
+++ pages/cards/[id]/index.js
@@ -1,27 +1,39 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function Details({ cards }) {
   const router = useRouter();
   const id = router ? router.query.id : null;
+  const [currentPage, setCurrentPage] = useState(id);
 
   function handleClickNext() {
     if (id < 77) {
+      setCurrentPage(parseInt(currentPage) + 1);
+      console.log(currentPage);
       router.push(`/cards/${parseInt(id) + 1}`);
     } else {
+      setCurrentPage(0);
       router.push(`/cards/0`);
     }
   }
 
   function handleClickPrev() {
     if (id > 0) {
+      setCurrentPage(parseInt(currentPage) - 1);
+      console.log(currentPage);
       router.push(`/cards/${parseInt(id) - 1}`);
     } else {
+      setCurrentPage(77);
       router.push(`/cards/77`);
     }
   }
-
+  useEffect(() => {
+    setCurrentPage(currentPage);
+    console.log("useEffect: ", currentPage);
+  }, [currentPage]);
   return (
     id < 78 && (
       <>
@@ -29,13 +41,13 @@ export default function Details({ cards }) {
           <Image
             width={100}
             height={180}
-            src={cards[id].image}
-            alt={cards[id].name}
+            src={cards[currentPage].image}
+            alt={cards[currentPage].name}
             priority={true}
           />
-          <figcaption>{cards[id].name}</figcaption>
+          <figcaption>{cards[currentPage].name}</figcaption>
         </figure>
-        <Link href={`/cards/${id}/detail`}>
+        <Link href={`/cards/${currentPage}/detail`}>
           <button type="button" aria-label="more details button">
             more details
           </button>
