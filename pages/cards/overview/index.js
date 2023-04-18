4ee38f00ba3216da817a6921e38954949e6f21bc
rabewtf <rabenklar@gmx.de>
2023-04-18 13:32:04 +0200

    make missing page dynamic

--- pages/cards/overview/index.js
+++ pages/cards/overview/index.js
@@ -1,16 +1,16 @@
 import Link from "next/link";
-import { useState, useEffect } from "react";
 import useStore from "../../../src/store/store";
 import BackButton from "../../../components/Backbutton/backbutton";
-export default function Description() {
-  const [hasMounted, setHasMounted] = useState(false);
 
-  useEffect(() => {
-    setHasMounted(true);
-  }, []);
-  if (!hasMounted) {
-    return null;
-  }
+export default function Overview() {
+  const cards = useStore((state) => state.allCards);
+  const allSuits = cards.map((card) => card.suit);
+  const suits = allSuits
+    .filter((suit, index) => {
+      return allSuits.indexOf(suit) === index;
+    })
+    .filter((item) => item !== undefined);
+
   return (
     <>
       <ul>
@@ -31,26 +31,17 @@ export default function Description() {
           </Link>
         </li>
         <ul>
-          <li>
-            <Link href={{ pathname: "/cards/overview/minor-arcana/wands" }}>
-              wands
-            </Link>
-          </li>
-          <li>
-            <Link href={{ pathname: "/cards/overview/minor-arcana/cups" }}>
-              cups
-            </Link>
-          </li>
-          <li>
-            <Link href={{ pathname: "/cards/overview/minor-arcana/pentacles" }}>
-              pentacles
-            </Link>
-          </li>
-          <li>
-            <Link href={{ pathname: "/cards/overview/minor-arcana/swords" }}>
-              swords
-            </Link>
-          </li>
+          {suits.map((suit) => {
+            return (
+              <li key={suit}>
+                <Link
+                  href={{ pathname: `/cards/overview/minor-arcana/${suit}` }}
+                >
+                  {suit}
+                </Link>
+              </li>
+            );
+          })}
         </ul>
       </ul>
       <BackButton />
