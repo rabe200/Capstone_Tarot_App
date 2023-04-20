b9f2071a0980d2f635823aa91f8c6aad4f7a7ea1
rabewtf <rabenklar@gmx.de>
2023-04-20 15:09:21 +0200

    add Layout.js and integrate to app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import GlobalStyle from "../styles";
-
+import Layout from "../components/Layout";
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </>
   );
 }
